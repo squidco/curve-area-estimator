372440f15ee0546b960a81d13db4ab2160fa212f
squidco <anthonypillow18@gmail.com>
2024-04-29 01:34:02 -0700

    Added prop validation to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function Card({title, children}) {
+export default function Card({ title, children }) {
   return (
-    <div className='p-1 m-1'>
-        <h2>{title}</h2>
-        <p>{children}</p>
+    <div className="p-1 m-1">
+      <h2>{title}</h2>
+      <p>{children}</p>
     </div>
-  )
+  );
 }
+
+Card.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.string,
+};
